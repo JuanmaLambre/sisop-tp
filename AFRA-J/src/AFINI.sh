1b19f6a5c20b33f21d64a03958b54a8633ecabfe
JuanmaLambre <juanmlambre@gmail.com>
2015-10-07 20:10:29 -0300

    AFINI script

--- AFRA-J/src/AFINI.sh
+++ AFRA-J/src/AFINI.sh
@@ -10,7 +10,7 @@ ERR_PERMISOS_DENEGADOS=4
 GRUPO="Grupo2"
 CONFDIR="$GRUPO/conf"
 AFINSTALCONF="$CONFDIR/AFINSTAL.cnfg"
-RALOG="Archivos/GraLog.sh"
+GRALOG="Archivos/GraLog.sh"
 
 
 # FUNCIONES:
@@ -21,7 +21,7 @@ function obtenerVariable {
 
 function checkEnvSet() {
     # TODO: REFACTOR
-    if [ ! -z $BINDIR ]
+    if ! [ -z $GRUPO -a -z $CONFDIR ] && ! [ -z $BINDIR -a -z $MAEDIR ] && ! [ -z $NOVEDIR -a -z $ACEPDIR ] && ! [ -z $PROCDIR -a -z $REPODIR ] && ! [ -z $LOGDIR -a -z $RECHDIR ]
     then
         echo "Ambiente ya inicializado. Para reiniciar termine la sesión e ingrese nuevamente."
         $GRALOG "AFINI" "Ambiente ya inicializado" "ERR"
@@ -49,11 +49,11 @@ function checkFilesExist() {
     fi
 }
 
-function setFilePermissions {
+function setRWPermissions {
     # Permisos para lectura:
     if [ ! -r $1 ]
     then
-        chmod -r $1
+        chmod +r $1
         if [ $? -ne 0 ]
         then
             echo "No se pudieron modificar los permisos de lectura para $1"
@@ -63,7 +63,7 @@ function setFilePermissions {
     fi
 
     # Permisos para escritura:
-    if [ ! -w $1 ]
+    if [ ! +w $1 ]
     then
         chmod -w $1
         if [ $? -ne 0 ]
@@ -75,11 +75,24 @@ function setFilePermissions {
     fi 
 }
 
+function setExePermissions {
+    # Permisos para ejecucion:
+    if [ ! -x $1 ]
+    then
+        chmod x $1
+        if [ $? -ne 0 ]
+        then
+            echo "No se pudieron modificar los permisos de ejecucion para $1"
+            $GRALOG "AFINI" "$1 no tiene permisos de ejecucion"  "ERR"
+            exit $ERR_PERMISOS_DENEGADOS
+        fi
+    fi
+}
 
 # EJECUCION:
 
 # (1) Verificar si el ambiente ya ha sido inicializado
-# checkEnvSet
+checkEnvSet
 
 # (2) Verificar que la instalacion está completa
 checkInstalation
@@ -95,20 +108,42 @@ UMBRAL="$MAEDIR/umbral.mae"
 checkFilesExist
 
 # (3) Verificar y seteo los permisos de lectura y escritura
-setFilePermissions $CPD
-setFilePermissions $CPA
-setFilePermissions $CDC
-setFilePermissions $AGENTES
-setFilePermissions $TLLAMA
-setFilePermissions $UMBRAL
+for file in `ls $MAEDIR`; do
+    setRWPermissions $file
+done
+
+for script in `ls $BINDIR`; do
+    setExePermissions $script
+done
 
 # (4) Inicializar el ambiente
 # Inicializo variables globales de Grupo2/conf/AFINSTAL.cnfg
-# TODO
-BINDIR=$(obtenerVariable "BINDIR")
+export GRUPO
+export CONFDIR
+export BINDIR=`obtenerVariable "BINDIR"`
+export MAEDIR
+export NOVEDIR=`obtenerVariable "NOVEDIR"`
+export ACEPDIR=`obtenerVariable "ACEPDIR"`
+export PROCDIR=`obtenerVariable "PROCDIR"`
+export REPODIR=`obtenerVariable "REPODIR"`
+export LOGDIR=`obtenerVariable "LOGDIR"`
+export RECHDIR=`obtenerVariable "RECHDIR"`
 
-# (5) Mostrar y grabar en el log variables y contenido (ver enunciado)
-# TODO
+# (5) Grabar en el log variables y contenido
+$GRALOG "AFINI" "Directorio de Configuración: $CONFDIR" "INFO"
+$GRALOG "AFINI" "Archivos en $CONFDIR: `ls -p $CONFDIR | grep -v /`" "INFO"
+$GRALOG "AFINI" "Directorio de Ejecutables: $BINDIR" "INFO"
+$GRALOG "AFINI" "Archivos en $BINDIR: `ls -p $BINDIR | grep -v /`" "INFO"
+$GRALOG "AFINI" "Directorio de Maestros y Tablas: $MAEDIR" "INFO"
+$GRALOG "AFINI" "Archivos en $MAEDIR: `ls -p $MAEDIR | grep -v /`" "INFO"
+$GRALOG "AFINI" "Directorio de recepción de archivos de llamadas: $NOVEDIR" "INFO"
+$GRALOG "AFINI" "Directorio de Archivos de llamadas Aceptados: $ACEPDIR" "INFO"
+$GRALOG "AFINI" "Directorio de Archivos de llamadas Sospechosas: $PROCDIR" "INFO"
+$GRALOG "AFINI" "Directorio de Archivos de Reportes de llamadas: $REPODIR" "INFO"
+$GRALOG "AFINI" "Directorio de Archivos de Log: $LOGDIR" "INFO"
+$GRALOG "AFINI" "Archivos en $LOGDIR: `ls -p $LOGDIR | grep -v /`" "INFO"
+$GRALOG "AFINI" "Directorio de Archivos Rechazados: $RECHDIR" "INFO"
+$GRALOG "AFINI" "Estado del Sistema: INICIALIZADO" "INFO"
 
 # (6) Ver si desea arrancar AFREC
 read -p "Desea correr AFREC? (s/n) " -n 1 -r
@@ -117,16 +152,17 @@ if [[ $REPLY =~ ^[Ss]$ ]]
 then
     if [[ $(ps -aux | grep -e "[0-9] [a-z]* AFREC") -ne "" ]]
     then
-        bash $BINDIR/Arrancar.sh &
+        bash $BINDIR/Arrancar.sh ./AFREC.sh &
+        PID_AFREC=$!
         echo "Para detenerlo ejecute el archivo Detener.sh"
     else
         echo "AFREC ya esta corriendo"
+        PID_AFREC=$( ps -x | grep -e "[0-9] [a-z]* AFREC" | sed 's:^ \([0-9]*\).*:\1:' )
     fi
-    PID_AFREC=$( ps -x | grep -e "[0-9] [a-z]* AFREC" | sed 's:^ \([0-9]*\).*:\1:' )
     $GRALOG "AFINI" "AFREC corriendo bajo el no.: $PID_AFREC" "INFO"
 else
-    echo "Puede comenzar el demonio AFREC ejecutando el archivo Arrancar.sh"
+    echo "Puede comenzar el demonio AFREC ejecutando \"Arrancar.sh AFREC.sh\""
 fi
 
-# (7) Cerrar el archivo de log y terminar proceso
-# ????
+
+exit 0
