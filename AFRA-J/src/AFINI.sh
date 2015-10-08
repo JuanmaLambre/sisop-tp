2f96374186d207f0f6f35dd2e6aef0a84a856ecb
JuanmaLambre <juanmlambre@gmail.com>
2015-10-07 23:06:14 -0300

    AFINI script

--- AFRA-J/src/AFINI.sh
+++ AFRA-J/src/AFINI.sh
@@ -20,11 +20,10 @@ function obtenerVariable {
 }
 
 function checkEnvSet() {
-    # TODO: REFACTOR
     if ! [ -z $GRUPO -a -z $CONFDIR ] && ! [ -z $BINDIR -a -z $MAEDIR ] && ! [ -z $NOVEDIR -a -z $ACEPDIR ] && ! [ -z $PROCDIR -a -z $REPODIR ] && ! [ -z $LOGDIR -a -z $RECHDIR ]
     then
         echo "Ambiente ya inicializado. Para reiniciar termine la sesión e ingrese nuevamente."
-        $GRALOG "AFINI" "Ambiente ya inicializado" "ERR"
+        $GRALOG $0 "Ambiente ya inicializado" "ERR"
         exit $ERR_AMBIENTE_INICIADO
     fi   
 }
@@ -34,7 +33,7 @@ function checkInstalation() {
     if [ ! -f "$GRUPO/AFINSTAL.cnfg" ]
     then
         echo "No se completo la instalacion. Por favor, ejecute AFINSTAL"
-        $GRALOG "AFINI" "Instalacion no completada" "ERR"
+        bash -x $GRALOG $0 "Instalacion no completada" "ERR"
         exit $ERR_INSTALACION_INCOMPLETA
     fi
 }
@@ -44,7 +43,7 @@ function checkFilesExist() {
     if [ ! [ -f "$CDP" -a -f "$CDA" -a -f "$CDC" -a -f "$AGENTES" -a -f "$TLLAMA" -a -f "$UMBRAL" ] ]
     then
         echo "Archivos faltantes. Por favor, ejecute AFINSTAL para continuar con la iniciacion"
-        $GRALOG "AFINI" "Archivos de instalacion faltantes" "ERR"
+        $GRALOG $0 "Archivos de instalacion faltantes" "ERR"
         exit $ERR_ARCHIVOS_FALTANTES
     fi
 }
@@ -57,19 +56,19 @@ function setRWPermissions {
         if [ $? -ne 0 ]
         then
             echo "No se pudieron modificar los permisos de lectura para $1"
-            $GRALOG "AFINI" "$1 no tiene permisos de lectura" "ERR"
+            $GRALOG $0 "$1 no tiene permisos de lectura" "ERR"
             exit $ERR_PERMISOS_DENEGADOS
         fi
     fi
 
     # Permisos para escritura:
-    if [ ! +w $1 ]
+    if [ ! -w $1 ]
     then
         chmod -w $1
         if [ $? -ne 0 ]
         then
             echo "No se pudieron modificar los permisos de escritura para $1"
-            $GRALOG "AFINI" "$1 no tiene permisos de escritura" "ERR"
+            $GRALOG $0 "$1 no tiene permisos de escritura" "ERR"
             exit $ERR_PERMISOS_DENEGADOS
         fi
     fi 
@@ -79,11 +78,11 @@ function setExePermissions {
     # Permisos para ejecucion:
     if [ ! -x $1 ]
     then
-        chmod x $1
+        chmod +x $1
         if [ $? -ne 0 ]
         then
             echo "No se pudieron modificar los permisos de ejecucion para $1"
-            $GRALOG "AFINI" "$1 no tiene permisos de ejecucion"  "ERR"
+            $GRALOG $0 "$1 no tiene permisos de ejecucion"  "ERR"
             exit $ERR_PERMISOS_DENEGADOS
         fi
     fi
@@ -94,11 +93,23 @@ function setExePermissions {
 # (1) Verificar si el ambiente ya ha sido inicializado
 checkEnvSet
 
+# Inicializo y/o exporto variables globales de Grupo2/conf/AFINSTAL.cnfg
+export GRUPO
+export CONFDIR
+export BINDIR=`obtenerVariable "BINDIR"`
+export MAEDIR=$(obtenerVariable "MAEDIR")
+export NOVEDIR=`obtenerVariable "NOVEDIR"`
+export ACEPDIR=`obtenerVariable "ACEPDIR"`
+export PROCDIR=`obtenerVariable "PROCDIR"`
+export REPODIR=`obtenerVariable "REPODIR"`
+export LOGDIR=`obtenerVariable "LOGDIR"`
+export RECHDIR=`obtenerVariable "RECHDIR"`
+export LOGEXT=`obtenerVariable "LOGEXT"`
+
 # (2) Verificar que la instalacion está completa
-checkInstalation
+#TODO checkInstalation
 
 # Seteo el path de MAEDIR y sus archivos para checkear que existan
-MAEDIR=$(obtenerVariable "MAEDIR")
 CPD="$MAEDIR/CdP.mae"
 CPA="$MAEDIR/CdA.mae"
 CDC="$MAEDIR/CdC.mae"
@@ -109,41 +120,29 @@ checkFilesExist
 
 # (3) Verificar y seteo los permisos de lectura y escritura
 for file in `ls $MAEDIR`; do
-    setRWPermissions $file
+    setRWPermissions $MAEDIR/$file
 done
 
 for script in `ls $BINDIR`; do
-    setExePermissions $script
+    setExePermissions $BINDIR/$script
 done
 
 # (4) Inicializar el ambiente
-# Inicializo variables globales de Grupo2/conf/AFINSTAL.cnfg
-export GRUPO
-export CONFDIR
-export BINDIR=`obtenerVariable "BINDIR"`
-export MAEDIR
-export NOVEDIR=`obtenerVariable "NOVEDIR"`
-export ACEPDIR=`obtenerVariable "ACEPDIR"`
-export PROCDIR=`obtenerVariable "PROCDIR"`
-export REPODIR=`obtenerVariable "REPODIR"`
-export LOGDIR=`obtenerVariable "LOGDIR"`
-export RECHDIR=`obtenerVariable "RECHDIR"`
-
 # (5) Grabar en el log variables y contenido
-$GRALOG "AFINI" "Directorio de Configuración: $CONFDIR" "INFO"
-$GRALOG "AFINI" "Archivos en $CONFDIR: `ls -p $CONFDIR | grep -v /`" "INFO"
-$GRALOG "AFINI" "Directorio de Ejecutables: $BINDIR" "INFO"
-$GRALOG "AFINI" "Archivos en $BINDIR: `ls -p $BINDIR | grep -v /`" "INFO"
-$GRALOG "AFINI" "Directorio de Maestros y Tablas: $MAEDIR" "INFO"
-$GRALOG "AFINI" "Archivos en $MAEDIR: `ls -p $MAEDIR | grep -v /`" "INFO"
-$GRALOG "AFINI" "Directorio de recepción de archivos de llamadas: $NOVEDIR" "INFO"
-$GRALOG "AFINI" "Directorio de Archivos de llamadas Aceptados: $ACEPDIR" "INFO"
-$GRALOG "AFINI" "Directorio de Archivos de llamadas Sospechosas: $PROCDIR" "INFO"
-$GRALOG "AFINI" "Directorio de Archivos de Reportes de llamadas: $REPODIR" "INFO"
-$GRALOG "AFINI" "Directorio de Archivos de Log: $LOGDIR" "INFO"
-$GRALOG "AFINI" "Archivos en $LOGDIR: `ls -p $LOGDIR | grep -v /`" "INFO"
-$GRALOG "AFINI" "Directorio de Archivos Rechazados: $RECHDIR" "INFO"
-$GRALOG "AFINI" "Estado del Sistema: INICIALIZADO" "INFO"
+$GRALOG $0 "Directorio de Configuración: $CONFDIR" "INFO"
+$GRALOG $0 "Archivos en $CONFDIR: `ls -p $CONFDIR | grep -v /`" "INFO"
+$GRALOG $0 "Directorio de Ejecutables: $BINDIR" "INFO"
+$GRALOG $0 "Archivos en $BINDIR: `ls -p $BINDIR | grep -v /`" "INFO"
+$GRALOG $0 "Directorio de Maestros y Tablas: $MAEDIR" "INFO"
+$GRALOG $0 "Archivos en $MAEDIR: `ls -p $MAEDIR | grep -v /`" "INFO"
+$GRALOG $0 "Directorio de recepción de archivos de llamadas: $NOVEDIR" "INFO"
+$GRALOG $0 "Directorio de Archivos de llamadas Aceptados: $ACEPDIR" "INFO"
+$GRALOG $0 "Directorio de Archivos de llamadas Sospechosas: $PROCDIR" "INFO"
+$GRALOG $0 "Directorio de Archivos de Reportes de llamadas: $REPODIR" "INFO"
+$GRALOG $0 "Directorio de Archivos de Log: $LOGDIR" "INFO"
+$GRALOG $0 "Archivos en $LOGDIR: `ls -p $LOGDIR | grep -v /`" "INFO"
+$GRALOG $0 "Directorio de Archivos Rechazados: $RECHDIR" "INFO"
+$GRALOG $0 "Estado del Sistema: INICIALIZADO" "INFO"
 
 # (6) Ver si desea arrancar AFREC
 read -p "Desea correr AFREC? (s/n) " -n 1 -r
@@ -159,9 +158,9 @@ then
         echo "AFREC ya esta corriendo"
         PID_AFREC=$( ps -x | grep -e "[0-9] [a-z]* AFREC" | sed 's:^ \([0-9]*\).*:\1:' )
     fi
-    $GRALOG "AFINI" "AFREC corriendo bajo el no.: $PID_AFREC" "INFO"
+    $GRALOG $0 "AFREC corriendo bajo el no.: $PID_AFREC" "INFO"
 else
-    echo "Puede comenzar el demonio AFREC ejecutando \"Arrancar.sh AFREC.sh\""
+    echo "Puede comenzar el demonio AFREC ejecutando nuevamente el inicializador."
 fi
 
 
